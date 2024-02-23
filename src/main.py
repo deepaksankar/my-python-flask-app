375da8300d4e96583fd4b311fd8e38ebc34ea809
sdeepakishere <sdeepakishere@gmail.com>
2024-02-23 15:08:01 +0000

    Adding 2 files to stage

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 
@@ -8,8 +8,15 @@ app = Flask(__name__)
 def index():
     return send_file('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
 
 if __name__ == "__main__":
     main()
+
+if __name__ == "__about__":
+    about()
