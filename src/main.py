2e22c592b82712699d4dd281a43eb27e48ec9741
sdeepakishere <sdeepakishere@gmail.com>
2024-03-07 16:07:06 +0000

    Adding URL shortner project

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -10,7 +11,18 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', num=5)
+
+@app.route("/your-url", methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {}
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as file:
+            json.dump(urls, file)
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('index'))
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
